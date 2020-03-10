2cb827655e5ff2ecfca1e6eba714a5c2af86c549
bhieu79@gmail.com <hieu.bd167174@sis.hust.edu.vn>
2020-03-10 10:01:26 +0700

    week2

---
+++ lab-week-2/Expression.php
@@ -0,0 +1,12 @@
+<html>
+<head> <title>Expression Example </title> </head>
+    <body>
+        <?php
+            $gr1 = 34;
+            $gr2 = 10;
+            $gr3 = 14;
+            $av = ($grade1 + $grade2 + $grade3) / 3;
+            print ("The average is $av");
+        ?> 
+    </body> 
+</html>
