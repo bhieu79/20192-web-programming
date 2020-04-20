fc24aba2aa5318f40a590a3a842a9fb80536b54c
hieu bui <bhieu79@gmail.com>
2020-04-20 20:58:31 +0700

    added week 6 homework

---
+++ lab-week-6/6-1/startsale.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Inventory Management</title>
+    </head>
+    <body>
+        <form action="sale.php" method="post">
+            Hammer: <input type="radio" name="Product" value="Hammer">
+            Screwdriver: <input type="radio" name="Product" value="Screwdriver">
+            Wrench: <input type="radio" name="Product" value="Wrench">
+            <br>
+            <input type="submit" name="" value="Submit">
+            <input type="reset" name="" value="Reset">
+        </form>
+    </body>
+</html>
